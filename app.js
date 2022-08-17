fd224c7b64b6dc0525e74c1708c26dcd9949c68b
Vinitpal <vs361017@gmail.com>
2022-08-17 18:57:28 +0530

    added frontend

--- app.js
+++ app.js
@@ -6,21 +6,11 @@ require("dotenv").config();
 const PORT = 3000;
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
-
-// home route
-app.get("/", (req, res) => {
-  res.send("Task Manager API");
-});
 
 // routes
 app.use("/api/v1/tasks", require("./routes/tasks"));
-
-//  app.get('/api/v1/tasks')        // - get all tasks
-//  app.post('/api/v1/tasks')       // - create a new task
-//  app.get('/api/v1/tasks/:id')    // - get single task
-//  app.patch('/api/v1/tasks/:id')  // - update task
-//  app.delete('/api/v1/tasks/:id') // - delete task
 
 const start = async () => {
   try {
